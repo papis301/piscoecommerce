0d58bf5432a5f64da15bad005f35f272aefc7a0d
papis301 <pfaye3@gmail.com>
2025-06-22 18:09:16 +0000

    ajout des articles dans la page d'accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\ImageP;
+use App\Repository\ProduitRepository;
 
 final class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(ProduitRepository $produitRepository): Response
     {
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'produits' => $produitRepository->findAll(),
         ]);
     }
 }
